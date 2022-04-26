7011968d821773d10fd6535e3327e5badca044dc
Josubin99 <zosehqmdl@gmail.com>
2022-04-27 06:37:48 +0900

    카테고리 화면 - 네트워크 통신구현(Android Coroutine, Retrofit 라이브러리 활용)

--- app/src/main/java/com/shoppi/app/ui/common/TextBindingAdapters.kt
+++ app/src/main/java/com/shoppi/app/ui/common/TextBindingAdapters.kt
@@ -7,7 +7,6 @@ import java.text.DecimalFormat
 import kotlin.math.roundToInt
 
 @BindingAdapter("priceAmount")
-
 fun applyPriceFormat(view:TextView, price:Int) {
     val decimalFormat = DecimalFormat("#,###")
     view.text = view.context.getString(R.string.unit_discount_currency, decimalFormat.format(price))
